bef051834ae5a334a0df2a7d4e7e919fb2291957
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-10-22 23:25:43 -0400

    final final final

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SliderContainerEl = document.querySelector('.gallery');
+const SliderContainerEl = document.querySelector('.track-wrapper');
 
 async function getDogImages(url){
     try{
@@ -12,11 +12,20 @@ async function getDogImages(url){
 }
 
 function displayDogImages(message){
-        for(let i = 0; i<6; i++){
+
+    const unList = document.createElement('ul');
+    unList.classList.add('track');
+    SliderContainerEl.appendChild(unList);
+        for(let i = 0; i<8; i++){
+            const listItem = document.createElement('li');
+            listItem.classList.add("track__item");
             const imgEl = document.createElement('img');
+            imgEl.classList.add("gallery__img");
             imgEl.src = message[i];
-            SliderContainerEl.appendChild(imgEl); 
+            listItem.appendChild(imgEl);
+            unList.appendChild(listItem);
         }
 }
+
 
 getDogImages('https://dog.ceo/api/breed/hound/images');
